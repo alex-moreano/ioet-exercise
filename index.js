3a98bf956dfd02f39829b6b68a20bd92a54bee13
Alex Moreano <alxstaleycobain@hotmail.com>
2022-12-05 14:04:02 -0500

    changed pathname in function, data successfully structured

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /************************************************* */
 const fs = require('fs');
-const filePath = './index.txt';
+const filePath = './data.txt';
 const dataFromFile = fs.readFileSync(filePath, 'utf-8');
 /*************************************************** */
 
@@ -15,8 +15,12 @@ const employeeSchedule = dataEmployees.map((dataEmployee)=>{
 
 const scheduleDaysHours = employeeSchedule.map((dataEmployee)=>{
     const {employeeName, employeeSchedule} = dataEmployee;
-    const scheduleDays = employeeSchedule.split('-').map((day)=>day.substring(0,2));
-    
+    const scheduleDays = employeeSchedule.split(',').map((day)=>day.substring(0,2));
+    const entryHours = employeeSchedule.split(',').map((hour)=>hour.substring(2,7));
+    const exitHours = employeeSchedule.split(',').map((hour)=>hour.substring(8,14));
+    return {employeeName, scheduleDays, entryHours, exitHours};
 });
 
-console.log(pairEmployeeSchedule);
+
+
+console.log(scheduleDaysHours);
