ff6435a44c8f8b7e3e719ce3ef6e2cb25c090d58
Alex Moreano <alxstaleycobain@hotmail.com>
2022-12-05 19:20:05 -0500

    creates structure and installed jest

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import { pairEmployees } from "./scripts/outputEmployees.js";
+import { employeesData } from "./scripts/getDataEmployees.js";
 
+console.log('This is the data you submitted of your employees: ')
+console.log(employeesData);
+console.log('Output:')
 console.log(pairEmployees);
--- scripts/outputEmployees.js
+++ scripts/outputEmployees.js
@@ -14,10 +14,17 @@ for (let i = 0; i < employeesData.length; i++) {
         let coincidence = 0;
         for (let k = 0; k < employee.scheduleDays.length; k++) {
             let matchDay = employeeTwo.scheduleDays.includes(employee.scheduleDays[k]);
-            let matchHours = employeeTwo.entryHours.includes(employee.entryHours[k]) && employeeTwo.exitHours.includes(employee.exitHours[k]);
-            let matchRange = checkInRange(employeeTwo.entryHours, employeeTwo.exitHours, employee.entryHours[k]) || checkInRange(employee.entryHours[k], employee.exitHours[k], employeeTwo.entryHours);
-            if((matchDay && matchHours) || (matchDay && matchRange || matchHours)){
-                coincidence++;
+            if(matchDay){
+                let matchHours = employeeTwo.entryHours.includes(employee.entryHours[k]) && employeeTwo.exitHours.includes(employee.exitHours[k]);
+                let matchRange = checkInRange(employeeTwo.entryHours, employeeTwo.exitHours, employee.entryHours[k]) 
+                                || checkInRange(employee.entryHours[k], employee.exitHours[k], employeeTwo.entryHours)
+                                || checkInRange(employee.entryHours[k], employee.exitHours[k], employeeTwo.entryHours)
+                                || checkInRange(employee.entryHours[k], employee.exitHours[k], employeeTwo.entryHours);
+                if(matchHours){
+                    coincidence++;
+                }else if(matchRange){
+                    coincidence ++;
+                }
             }
         }
         pairEmployees[`${employee.employeeName}-${employeeTwo.employeeName}`] = coincidence;
